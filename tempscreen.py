6a90593183ba2eaf4d33bfc549f03928b719d545
Timéo <116905321+TimeoDoom@users.noreply.github.com>
2023-12-02 18:48:32 +0100

    Update tempscreen.py

--- tempscreen.py
+++ tempscreen.py
@@ -7,17 +7,16 @@ import math
 # Pin de la thermistance
 thermistor = machine.ADC(28)
 
-# ecran lcd 
+# Ecran lcd  
 I2C_ADDR = 0x27
 I2C_NUM_ROWS = 2
 I2C_NUM_COLS = 16
 i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
 lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
-# Fonction temperature
+# Fonction temperature et affichage
 
-    
-while True:
+def screenTemp():
     temperature_value = thermistor.read_u16()
     Vr = 3.3 * float(temperature_value) / 65535
     Rt = 10000 * Vr / (3.3 - Vr)
@@ -28,3 +27,7 @@ while True:
     lcd.backlight_on()
     lcd.move_to(0,0)
     lcd.putstr(f"La temp est de \n{cel} C")
+
+    
+while True:
+    screenTemp()
